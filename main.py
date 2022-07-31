cccb8be036d260c64803d792f4e078fb161602cf
chauncey <2268136725@qq.com>
2022-07-31 11:17:02 +0800

    first commit: get rss content with python

---
+++ main.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+
+from requests import get
+import json
+
+
+def get_data():
+    with open("data.json", 'r') as f:
+        rss_url = json.load(f)
+    return rss_url
+
+
+def get_rss(url):
+    resp = get(url)
+    return resp.content.decode('UTF-8')
+
+
+def write_rss_xml(name, xml):
+    with open(name + ".xml", 'w+', encoding='UTF-8') as f:
+        f.write(xml)
+
+
+def run():
+    rss_url = get_data()
+    for name, value in rss_url.items():
+        write_rss_xml(name, get_rss(value))
+
+
+if __name__ == '__main__':
+    run()
