d88cddbb89340d7058e276ff61462b53e6c35c53
chauncey <2268136725@qq.com>
2022-09-03 20:24:52 +0800

    feat: 实现Jackett Indexer，基本完成xml解析

--- jackett.py
+++ jackett.py
@@ -1,13 +1,15 @@
 import requests
 
+
 class Jackett():
     def __init__(self):
-        '''
+        '''获取 Jackett Cookie
 
         '''
         self.base_url = "http://jackett.52bilibili.com"
         password = "jackett"
         self.cookie = ""
+        self.apikey = "ue9ko6yfbhpijxpcqmsmcu4k3a8oo0mj"
         payload = {'password': password}
         url = self.base_url + "/UI/Dashboard"
         s = requests.Session()
@@ -15,6 +17,21 @@ class Jackett():
         self.cookie = s.cookies
 
     def get_indexers(self):
+        '''获取 Jackett Indexer
+
+        :return:
+        '''
         url = self.base_url + "/api/v2.0/indexers?configured=true"
         resp = requests.get(url, cookies=self.cookie)
         return resp.json()
+
+    def search(self, indexer, keywords, categories=5000):
+        url = self.base_url + "/api/v2.0/indexers/{}/results/torznab/".format(indexer)
+        payload = {'apikey': self.apikey, 't': 'search', 'q': keywords, 'cat': categories}
+        resp = requests.get(url, params=payload)
+        return resp.content.decode('UTF-8')
+
+
+if __name__ == '__main__':
+    jackett = Jackett()
+    print(jackett.search('acgrip', "异世界舅舅"))
---
+++ parser.py
@@ -0,0 +1,121 @@
+import xml.sax
+
+
+class Parser(xml.sax.handler.ContentHandler):
+    def __init__(self):
+        self._charBuffer = []
+        self.tag = ""
+        self.depth = []     # 记录树
+        self.result = []
+        self.title = ""
+        self.guid = ""
+        self.type = ""
+        self.site = ""
+        self.page = ""
+        self.date = ""
+        self.size = ""
+        self.grabs = 0
+        self.link = ""
+        self.category = []
+        self.seeders = 0
+        self.peers = 0
+        self.downloadvolumefactor = 0
+        self.uploadvolumefactor = 0
+
+    def startElement(self, name, attrs):
+        # print("这是startElement: ", name)
+        self._charBuffer = []
+        self.tag = name
+        self.depth.append(name)
+        if self.tag == "item":
+            # print("开始解析xml内容")
+            self.clearField()
+
+    def _flushCharBuffer(self):
+        s = ''.join(self._charBuffer)
+        # print(self._charBuffer)
+        return s
+
+    def characters(self, content):
+        self._charBuffer.append(content)
+        # print(self._charBuffer)
+        # if self.depth[-2:] == "item":
+        if 1:
+            if self.tag == "title":
+                self.title = self._charBuffer
+            elif self.tag == "guid":
+                self.guid = self._charBuffer
+            elif self.tag == "jackettindexer":
+                self.site = self._charBuffer
+            elif self.tag == "type":
+                self.type = self._charBuffer
+            elif self.tag == "comments":
+                self.page = self._charBuffer
+            elif self.tag == "pubData":
+                self.date = self._charBuffer
+            elif self.tag == "size":
+                self.size = self._charBuffer
+            elif self.tag == "grabs":
+                self.grabs = self._charBuffer
+            elif self.tag == "link":
+                self.link = self._charBuffer
+            elif self.tag == "category":
+                self.category.append(self._charBuffer)
+            else:
+                print("未处理元素")
+        else:
+            pass
+
+    def endElement(self, name):
+        self.tag = name
+        # print("这是endElement: ", name)
+        if self.tag == self.depth[-1:]:
+            self.depth.pop()
+        if self.tag == "item":
+            item = {
+                "title": self.title,
+                "guid": self.guid,
+                "type": self.type,
+                "site": self.site,
+                "page": self.page,
+                "date": self.date,
+                "size": self.size,
+                "grab": self.grabs,
+                "link": self.link,
+                "category": self.category,
+                "seeders": self.seeders,
+                "peers": self.peers,
+                "downloadvolumefactor": self.downloadvolumefactor,
+                "uploadvolumefactor": self.uploadvolumefactor
+            }
+            self.result.append(item)
+            self.clearField()
+        print(self.result)
+
+    def clearField(self):
+        self.title = ""
+        self.guid = ""
+        self.type = ""
+        self.site = ""
+        self.page = ""
+        self.date = ""
+        self.size = ""
+        self.grabs = 0
+        self.link = ""
+        self.category = []
+        self.seeders = 0
+        self.peers = 0
+        self.downloadvolumefactor = 0
+        self.uploadvolumefactor = 0
+
+
+if __name__ == '__main__':
+    parser = xml.sax.make_parser()
+    # 关闭命名空间
+    parser.setFeature(xml.sax.handler.feature_namespaces, 0)
+
+    # 重写 ContextHandler
+    Handler = Parser()
+    parser.setContentHandler(Handler)
+
+    parser.parse("test.xml")
