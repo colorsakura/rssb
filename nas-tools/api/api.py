9bfba4c35c8dc91b17c3f3a32ffa7dd08174a381
chauncey <2268136725@qq.com>
2022-09-18 13:51:44 +0800

    update

--- nas-tools/api/api.py
+++ nas-tools/api/api.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from bangumi.bangumi import BangumiTV
+from tmdb.tmdb import Tmdb
 
 api = FastAPI()
 bangumi = BangumiTV()
+tmdb = Tmdb()
 
 
 @api.get('/')
@@ -10,13 +12,18 @@ async def index():
     return {"code": 0, "data": "Hello World!"}
 
 
-@api.get('/search/{keywords}')
-async def search(keywords: str):
-    return bangumi.search(keywords)
+@api.get('/search/')
+async def search(q: str, limit=8):
+    return tmdb.search(q)
 
 
 @api.get('/login')
-async def login():
+async def login(user: str, password: str):
+    return {}
+
+
+@api.get('/logout')
+async def logout():
     return {}
 
 
@@ -25,7 +32,30 @@ async def register():
     pass
 
 
+@api.get('/trend')
+async def get_trend_list():
+    return {}
+
+
+@api.get('/all')
+async def get_all(page=1, limit=10):
+    return {}
+
+
+@api.get('/detail/{tmdbid}')
+def get_detail(tmdbid: int):
+    return {}
+
+
+@api.post('/subscribe/{tmdbid}')
+async def subscribe(tmdbid: int):
+    return {}
+
+
 if __name__ == '__main__':
     import uvicorn
+    import os
 
+    if os.getenv('Debug'):
+        pass
     uvicorn.run(app="api:api", debug=True, reload=True)
--- nas-tools/api/tmdb/tmdb.py
+++ nas-tools/api/tmdb/tmdb.py
@@ -1,17 +1,66 @@
-from tmdbv3api import TMDb, TV, Season
+import requests
+
+
+class Tmdb():
+    def __init__(self):
+        self.baseurl = 'https://api.themoviedb.org/3'
+        self.header = {
+
+        }
+        self.apikey = 'b35d09cd066aaa90e9f51448fd78cd90'
+        self.imgurl = 'https://image.tmdb.org/t/p/original'
+
+    def init(self):
+        pass
+
+    def request(self, url, keyword):
+        payload = {
+            "api_key": self.apikey,
+            "language": "zh-CN",
+            "query": keyword,
+            "include_adult": "true",
+        }
+        return requests.get(url, params=payload, headers=self.header)
+
+    def tv(self, keyword):
+        url = self.baseurl + '/search/tv'
+        resp = self.request(url, keyword).json()
+        items = resp['results']
+        for item in items:
+            if item['backdrop_path'] != None:
+                item['backdrop_path'] = self.imgurl + item['backdrop_path']
+            if item['poster_path'] != None:
+                item['poster_path'] = self.imgurl + item['poster_path']
+        return items
+
+    def movie(self, keyword):
+        url = self.baseurl + "/search/movie"
+        resp = self.request(url, keyword).json()
+        items = resp['results']
+        for item in items:
+            if item['backdrop_path'] != None:
+                item['backdrop_path'] = self.imgurl + item['backdrop_path']
+            if item['poster_path'] != None:
+                item['poster_path'] = self.imgurl + item['poster_path']
+        return items
+
+    def search(self, keyword):
+        tv = self.tv(keyword)
+        movie = self.movie(keyword)
+        return tv + movie
+
+    def search_by_tmdbid(self, tmdbid):
+        pass
+
 
 if __name__ == '__main__':
-    tmdb = TMDb()
-    tmdb.api_key = 'b35d09cd066aaa90e9f51448fd78cd90'
-    tmdb.language = 'zh-CN'
-    tmdb.debug = True
+    tmdb = Tmdb()
 
-    tv = TV()
-
-    resp = tv.search("关于我转生变成史莱姆这档事 ")
-    for result in resp:
-        print(result)
-
-    season = Season()
-    show_season = season.details(82684,1)
-    print(show_season)
+    r = tmdb.search("总动员 ")
+    print(r)
+    # for result in resp:
+    #     print(result)
+    #
+    # season = Season()
+    # show_season = season.details(82684, 1)
+    # print(show_season)
